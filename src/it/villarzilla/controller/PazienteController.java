538aba8d3f8879aed062628ed2b479f6a625283e
vins87 <vins87@vins-asus>
2016-06-09 17:04:47 +0200

    Added frontend index

--- src/it/villarzilla/controller/PazienteController.java
+++ src/it/villarzilla/controller/PazienteController.java
@@ -1,5 +1,34 @@
 package it.villarzilla.controller;
 
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import java.util.List;
+
+import it.villarzilla.facade.PazienteFacade;
+import it.villarzilla.model.Paziente;
+
+@ManagedBean(name="pazienteController")
 public class PazienteController {
 
+	@EJB
+	private PazienteFacade pazienteFacade;
+	
+	public PazienteController(){
+		
+	}
+	
+	public List<Paziente> getList() {
+		return pazienteFacade.getAllPaziente();
+	}
+	
+	//INIZIO GETTER AND SETTER
+	public PazienteFacade getPazienteFacade() {
+		return pazienteFacade;
+	}
+
+	public void setPazienteFacade(PazienteFacade pazienteFacade) {
+		this.pazienteFacade = pazienteFacade;
+	}
+	
+	
 }
