b948e0e2c42b9c854e2fda0ea37cafd4ad042905
vins87 <vins87@vins-asus>
2016-06-13 23:06:58 +0200

    AmministrazioneController login works

--- src/it/villarzilla/controller/AmministrazioneController.java
+++ src/it/villarzilla/controller/AmministrazioneController.java
@@ -4,6 +4,8 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 
 import it.villarzilla.facade.AmministratoreFacade;
+import it.villarzilla.manager.AmministrazioneManager;
+import it.villarzilla.model.Amministratore;
 
 @ManagedBean(name="amministrazioneController")
 public class AmministrazioneController {
@@ -11,11 +13,21 @@ public class AmministrazioneController {
 	private String nome;
 	private String password;
 	
+	private AmministrazioneManager sessione;
+	
 	@EJB
 	public AmministratoreFacade amministratoreFacade;
 	
 	public AmministrazioneController(){
 		
+	}
+	
+	public String accedi(Amministratore a){
+		if(a.getPassword().equals(this.password)){
+			this.sessione.login(a);
+			return "/portaleAmministratore/portaleAdmin.xhtml?faces-redirect=true";
+		}
+		return "/loginAdmin.xhtml";
 	}
 
 	public String getNome() {
